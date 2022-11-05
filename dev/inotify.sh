ca7da323599eceb189e25bf388305164a943454e
ijortengab <m_roji28@yahoo.com>
2022-11-05 11:34:41 +0700

    Update dev/inotify.sh.

--- dev/inotify.sh
+++ dev/inotify.sh
@@ -2,9 +2,10 @@
 # How to Use:
 [ -z "$1" ] && { echo Argument Directory required; exit; }
 [ -d "$1" ] || { echo Directory not exists; exit; }
+wait_before_execute="$2"
+[ -n "$wait_before_execute" ] || { wait_before_execute=1; }
 dir=$(realpath "$1")
 this_file=$(realpath "$0")
-
 
 while true; do
     n=3
@@ -23,9 +24,19 @@ while true; do
             sleep .5
             echo This file
         else
-            [[ -x "$LINE" ]] &&  {
-                echo Executing....
-                sleep .5
+            [[ -x "$dir/$LINE" ]] &&  {
+                echo Execute
+                if [ $wait_before_execute -gt 1 ];then
+                    n="$wait_before_execute"
+                    until [[ $n == 0 ]]; do
+                        printf "\r\033[K"  >&2
+                        echo -n Waiting $n...  >&2
+                        let n--
+                        sleep 1
+                    done
+                else
+                    sleep $wait_before_execute
+                fi
                 "$dir"/"$LINE"
             }
         fi
