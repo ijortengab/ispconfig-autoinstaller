5fab952d235c17068e344f0b6df18184dc9df6f0
ijortengab <m_roji28@yahoo.com>
2022-11-17 15:47:04 +0700

    Update 20221117-154704 at vmgunawan6debian11b.

--- dev/back.sh
+++ dev/back.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ $(uname | cut -c1-6) == "CYGWIN" ]];then
-    if [[ "$0" =~ back\.sh$ ]];then
-        echo 'Cara penggunaan yang benar adalah `. back.sh`'
-    else
-        cd $PWD/../
-        git switch master
+if [[ "$0" =~ back\.sh$ ]];then
+    echo 'Cara penggunaan yang benar adalah `. back.sh`'
+else
+    cd $PWD/../
+    git switch master
+    if [ $? -eq 1 ];then
+        cd -
     fi
 fi
