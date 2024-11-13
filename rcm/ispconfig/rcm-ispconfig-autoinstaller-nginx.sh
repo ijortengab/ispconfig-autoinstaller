7bb4f17147141824346eaae673dab24960877596
ijortengab <m_roji28@yahoo.com>
2024-11-13 18:42:37 +0700

    Bugfix while pass option ke command rcm-nginx-virtual-host-autocreate-php.

--- rcm/ispconfig/rcm-ispconfig-autoinstaller-nginx.sh
+++ rcm/ispconfig/rcm-ispconfig-autoinstaller-nginx.sh
@@ -772,16 +772,22 @@ root="$prefix/interface/web"
 code 'root="'$root'"'
 filename="$ISPCONFIG_NGINX_CONFIG_FILE"
 code 'filename="'$filename'"'
-server_name="$ISPCONFIG_FQDN_LOCALHOST"
-code 'server_name="'$server_name'"'
+url_scheme=http
+url_port=80
+url_host="$ISPCONFIG_FQDN_LOCALHOST"
+code 'url_scheme="'$url_scheme'"'
+code 'url_host="'$url_host'"'
+code 'url_port="'$url_port'"'
 ____
 
 INDENT+="    " \
 rcm-nginx-virtual-host-autocreate-php $isfast --root-sure \
     --root="$root" \
     --filename="$filename" \
-    --server-name="$server_name" \
     --fastcgi-pass="unix:${socket_filename}" \
+    --url-host="$url_host" \
+    --url-scheme="$url_scheme" \
+    --url-port="$url_port" \
     ; [ ! $? -eq 0 ] && x
 
 chapter Mengecek subdomain '`'$ISPCONFIG_FQDN_LOCALHOST'`'.
