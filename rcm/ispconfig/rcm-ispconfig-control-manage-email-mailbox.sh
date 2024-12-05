bcffaea6eba0e7cf83d893cfa80b82809e8ef76a
ijortengab <m_roji28@yahoo.com>
2024-12-05 12:45:43 +0700

    [rcm-ispconfig-control-manage-email-mailbox.sh] Parse option menggunakan --with-end-options-double-dash. Menambah deskripsi option double dash. Menambah command dependency: rcm-php-ispconfig. Memindahkan verifikasi dependency tepat dibawah title. Update function: mailboxCredential, file yang dibuat murni berisi password, bukan lagi shell script yang mendefine password. Menghapus function: isFileExists, remoteUserCredentialIspconfig, getMailUserIdIspconfigByEmail, isEmailIspconfigExist, insertEmailIspconfig. Membuat function: isExists, create. Membuat global variable tempfile. Menghapus global variable php, tidak ada lagi embed PHP script, digantikan eksekusi command rcm-php-ispconfig.

--- rcm/ispconfig/rcm-ispconfig-control-manage-email-mailbox.sh
+++ rcm/ispconfig/rcm-ispconfig-control-manage-email-mailbox.sh
@@ -14,6 +14,13 @@ while [[ $# -gt 0 ]]; do
         --name=*) name="${1#*=}"; shift ;;
         --name) if [[ ! $2 == "" && ! $2 =~ (^--$|^-[^-]|^--[^-]) ]]; then name="$2"; shift; fi; shift ;;
         --root-sure) root_sure=1; shift ;;
+        --) shift
+            while [[ $# -gt 0 ]]; do
+                case "$1" in
+                    *) _new_arguments+=("$1"); shift ;;
+                esac
+            done
+            ;;
         --[^-]*) shift ;;
         *) _new_arguments+=("$1"); shift ;;
     esac
@@ -57,6 +64,8 @@ Options:
         The name of mailbox.
    --domain
         The domain of mailbox.
+   --
+        Every arguments after double dash will pass to \`rcm-php-ispconfig soap mail_user_add\` command.
 
 Global Options:
    --fast
@@ -78,11 +87,13 @@ Environment Variables:
 
 Dependency:
    rcm-ispconfig-control-manage-domain:`printVersion`
+   rcm-php-ispconfig:`printVersion`
    php
    mysql
 
 Download:
    [rcm-ispconfig-control-manage-domain](https://github.com/ijortengab/ispconfig-autoinstaller/raw/master/rcm/ispconfig/rcm-ispconfig-control-manage-domain.sh)
+   [rcm-php-ispconfig](https://github.com/ijortengab/ispconfig-autoinstaller/raw/master/rcm/php/rcm-php-ispconfig.php)
 EOF
 }
 
@@ -94,6 +105,11 @@ EOF
 title rcm-ispconfig-control-manage-email-mailbox
 ____
 
+# Dependency.
+while IFS= read -r line; do
+    [[ -z "$line" ]] || command -v `cut -d: -f1 <<< "${line}"` >/dev/null || { error Unable to proceed, command not found: '`'$line'`'.; x; }
+done <<< `printHelp 2>/dev/null | sed -n '/^Dependency:/,$p' | sed -n '2,/^\s*$/p' | sed 's/^ *//g'`
+
 if [ -z "$root_sure" ];then
     chapter Mengecek akses root.
     if [[ "$EUID" -ne 0 ]]; then
@@ -104,148 +120,74 @@ if [ -z "$root_sure" ];then
     ____
 fi
 
-# Dependency.
-while IFS= read -r line; do
-    [[ -z "$line" ]] || command -v `cut -d: -f1 <<< "${line}"` >/dev/null || { error Unable to proceed, command not found: '`'$line'`'.; x; }
-done <<< `printHelp 2>/dev/null | sed -n '/^Dependency:/,$p' | sed -n '2,/^\s*$/p' | sed 's/^ *//g'`
-
 # Functions.
-isFileExists() {
-    # global used:
-    # global modified: found, notfound
-    # function used: __
-    if [[ -f "$1" && ! -s "$1" ]];then
-        __ Empty file detected.
-        __; magenta rm "$1"; _.
-        rm "$1"
-    fi
-    found=
-    notfound=
-    if [ -f "$1" ];then
-        __ File '`'$(basename "$1")'`' ditemukan.
-        found=1
-    else
-        __ File '`'$(basename "$1")'`' tidak ditemukan.
-        notfound=1
-    fi
-}
-remoteUserCredentialIspconfig() {
-    local ISPCONFIG_REMOTE_USER_PASSWORD ISPCONFIG_REMOTE_USER_NAME
-    local user="$1"
-    local path=/usr/local/share/ispconfig/credential/remote/$user
-    isFileExists "$path"
-    [ -n "$notfound" ] && fileMustExists "$path"
-    # Populate.
-    . "$path"
-    ispconfig_remote_user_name=$ISPCONFIG_REMOTE_USER_NAME
-    ispconfig_remote_user_password=$ISPCONFIG_REMOTE_USER_PASSWORD
-}
-getMailUserIdIspconfigByEmail() {
-    local email="$1"@"$2"
-    __ Execute SOAP '`'mail_user_get'`'.
-    arguments="$(php -r "echo serialize([
-        'session_id' => null,
-        'params' => [
-            'email' => '${email}',
-        ],
-    ]);")"
-    stdout=$(php -r "$php" mail_user_get "$options" "$credentials" "$arguments")
-    __ Standard Output.
-    magenta "$stdout"; _.
-    if php -r "$php" is_empty <<< "$stdout";then
-        return
-    else
-        echo `php -r "$php" get mailuser_id <<< "$stdout"`
-    fi
-}
-isEmailIspconfigExist() {
-    mailuser_id=$(getMailUserIdIspconfigByEmail "$1" "$2")
-    if [ -n "$mailuser_id" ];then
-        return 0
-    fi
-    return 1
-}
 mailboxCredential() {
+    # global modified $password
     local host="$1"
     local user="$2"
-    if [ -f /usr/local/share/credential/mailbox/$host/$user ];then
-        local MAILBOX_USER_PASSWORD
-        . /usr/local/share/credential/mailbox/$host/$user
-        mailbox_user_password=$MAILBOX_USER_PASSWORD
+    [ -n "$host" ] || { error Variable host is required; x; }
+    [ -n "$user" ] || { error Variable user is required; x; }
+
+    local path="/usr/local/share/credential/mailbox/${host}/${user}"
+    local dirname="/usr/local/share/credential/mailbox/${host}"
+    __; magenta 'path="'$path'"'; _.
+    if [ -z "$password" ];then
+        password=$(pwgen 9 -1vA0B)
+    fi
+    if [ -f "$path" ];then
+        local _password=$(<"$path")
+        if [[ ! "$_password" == "$password" ]];then
+            echo "$password" > "$path"
+        fi
     else
-        mailbox_user_password=$(pwgen 9 -1vA0B)
-        mkdir -p /usr/local/share/credential/mailbox/$host/
-        cat << EOF > /usr/local/share/credential/mailbox/$host/$user
-MAILBOX_USER_PASSWORD=$mailbox_user_password
-EOF
-        chmod 0500 /usr/local/share/credential
-        chmod 0500 /usr/local/share/credential/mailbox
-        chmod 0500 /usr/local/share/credential/mailbox/$host
-        chmod 0400 /usr/local/share/credential/mailbox/$host/$user
+        mkdir -p "$dirname"
+        echo "$password" > "$path"
+        chmod 0500 "$dirname"
+        chmod 0400 "$path"
     fi
 }
-insertEmailIspconfig() {
-    local user="$1"
-    local host="$2"
-    __ Mengecek credentials Mailbox.
-    mailboxCredential $host $user
-    if [[ -z "$mailbox_user_password" ]];then
-        __; red Informasi credentials tidak lengkap: '`'/usr/local/share/credential/mailbox/$host/$user'`'.; x
+isExists() {
+    # global $user, $host, $tempfile
+    # global modified $mailuser_id
+    [ -n "$user" ] || { error Variable user is required; x; }
+    [ -n "$host" ] || { error Variable host is required; x; }
+    [ -n "$tempfile" ] || { error Variable tempfile is required; x; }
+    local email="${user}@${host}"
+    code rcm-php-ispconfig soap --empty-array-is-false mail_user_get --email='"'$email'"'
+    rcm-php-ispconfig soap --empty-array-is-false mail_user_get --email="$email" 2>&1 &> "$tempfile"
+    local exit_code=$?
+    if [ $exit_code -eq 0 ];then
+        mailuser_id=$(rcm-php-ispconfig echo [0][mailuser_id] < "$tempfile")
+        __; magenta mailuser_id=$mailuser_id; _.
     else
-        __; magenta mailbox_user_password="$mailbox_user_password"; _.
+        while IFS= read line; do e "$line"; _.; done < "$tempfile"
     fi
-    __ Execute SOAP '`'mail_user_add'`'.
-    arguments="$(php -r "echo serialize([
-        'session_id' => null,
-        'client_id' => 0,
-        'params' => [
-            'server_id' => '1',
-            'email' => '$user@$host',
-            'login' => '$user@$host',
-            'password' => '$mailbox_user_password',
-            'name' => '$user',
-            'uid' => '5000',
-            'gid' => '5000',
-            'maildir' => '/var/vmail/$host/$user',
-            'maildir_format' => 'maildir',
-            'quota' => '0',
-            'cc' => '',
-            'forward_in_lda' => 'y',
-            'sender_cc' => '',
-            'homedir' => '/var/vmail',
-            'autoresponder' => 'n',
-            'autoresponder_start_date' => NULL,
-            'autoresponder_end_date' => NULL,
-            'autoresponder_subject' => '',
-            'autoresponder_text' => '',
-            'move_junk' => 'Y',
-            'purge_trash_days' => 0,
-            'purge_junk_days' => 0,
-            'custom_mailfilter' => NULL,
-            'postfix' => 'y',
-            'greylisting' => 'n',
-            'access' => 'y',
-            'disableimap' => 'n',
-            'disablepop3' => 'n',
-            'disabledeliver' => 'n',
-            'disablesmtp' => 'n',
-            'disablesieve' => 'n',
-            'disablesieve-filter' => 'n',
-            'disablelda' => 'n',
-            'disablelmtp' => 'n',
-            'disabledoveadm' => 'n',
-            'disablequota-status' => 'n',
-            'disableindexer-worker' => 'n',
-            'last_quota_notification' => NULL,
-            'backup_interval' => 'none',
-            'backup_copies' => '1',
-        ],
-    ]);")"
-    mailuser_id=$(php -r "$php" mail_user_add "$options" "$credentials" "$arguments")
-    if [ -n "$mailuser_id" ];then
-        return 0
+    return $exit_code
+}
+create() {
+    # global $user, $host, $tempfile
+    # global modified $mailuser_id
+    [ -n "$user" ] || { error Variable user is required; x; }
+    [ -n "$host" ] || { error Variable host is required; x; }
+    [ -n "$tempfile" ] || { error Variable tempfile is required; x; }
+    local email="${user}@${host}"
+
+    ____; client_id=$(INDENT+="    " rcm-ispconfig-control-manage-client $isfast get-client-id --username "$domain")
+    code 'client_id="'$client_id'"'
+    [ -n "$client_id" ] || client_id=0
+    code 'client_id="'$client_id'"'
+    mailboxCredential $host $user
+    __; magenta 'password="'$password'"'; _.
+    code rcm-php-ispconfig soap mail_user_add '"'$client_id'"' --server-id='"'1'"' --email='"'$email'"' --password='"'$password'"' "$@"
+    rcm-php-ispconfig soap mail_user_add "$client_id" --server-id="1" --email="$email" --password="$password" "$@" 2>&1 &> "$tempfile"
+    local exit_code=$?
+    if [ $exit_code -eq 0 ];then
+        mailuser_id=$(cat "$tempfile" | rcm-php-ispconfig echo)
+        __; magenta mailuser_id=$mailuser_id; _.
+    else
+        while IFS= read line; do e "$line"; _.; done < "$tempfile"
     fi
-    return 1
+    return $exit_code
 }
 
 # Require, validate, and populate value.
@@ -265,141 +207,27 @@ fi
 code 'name="'$name'"'
 code 'ispconfig_domain_exists_sure="'$ispconfig_domain_exists_sure'"'
 code 'ispconfig_soap_exists_sure="'$ispconfig_soap_exists_sure'"'
+tempfile=
 ____
 
 if [ -z "$ispconfig_domain_exists_sure" ];then
     INDENT+="    " \
     rcm-ispconfig-control-manage-domain $isfast --root-sure \
-        isset \
         --domain="$domain" \
-        ; [ $? -eq 0 ] && ispconfig_domain_exists_sure=1
-    if [ -n "$ispconfig_domain_exists_sure" ];then
-        __; green Domain is exists.; _.
-    else
-        __; red Domain is not exists.; x
-    fi
+        ; [ ! $? -eq 0 ] && x
 fi
-
-php=$(cat <<'EOF'
-$mode = $_SERVER['argv'][1];
-switch ($mode) {
-    case 'is_empty':
-    case 'get':
-        $stdin = '';
-        while (FALSE !== ($line = fgets(STDIN))) {
-           $stdin .= $line;
-        }
-        $result = @unserialize($stdin);
-        if ($result === false) {
-            echo('Unserialize failed: '. $stdin.PHP_EOL);
-            exit(1);
-        }
-        break;
-    case 'login':
-    case 'mail_user_get':
-    case 'mail_user_add':
-        $options = unserialize($_SERVER['argv'][2]);
-        // Populate variable $username, $password.
-        $credentials = unserialize($_SERVER['argv'][3]);
-        extract($credentials);
-        break;
-}
-switch ($mode) {
-    case 'get':
-        $key = $_SERVER['argv'][2];
-        $result = array_shift($result);
-        if (array_key_exists($key, $result)) {
-            echo $result[$key];
-        }
-        break;
-    case 'is_empty':
-        empty($result) ? exit(0) : exit(1);
-        break;
-    case 'login':
-        $client = new SoapClient(null, $options);
-        try {
-            if($session_id = $client->login($username, $password)) {
-                echo 'Logged successfull. Session ID:'.$session_id.PHP_EOL;
-            }
-            if($client->logout($session_id)) {
-                echo "Logged out.".PHP_EOL;
-            }
-            exit(0);
-        } catch (SoapFault $e) {
-            fwrite(STDERR, 'SOAP Error: '.$e->getMessage().PHP_EOL);
-            exit(1);
-        }
-        break;
-    case 'mail_user_add':
-        // Populate variable $session_id, $client_id, $params.
-        $arguments = unserialize($_SERVER['argv'][4]);
-        extract($arguments);
-        $client = new SoapClient(null, $options);
-        try {
-            if($session_id = $client->login($username, $password)) {
-            }
-            //* Set the function parameters.
-            $mailuser_id = $client->mail_user_add($session_id, $client_id, $params);
-            echo $mailuser_id;
-            if($client->logout($session_id)) {
-            }
-        } catch (SoapFault $e) {
-            fwrite(STDERR, 'SOAP Error: '.$e->getMessage().PHP_EOL);
-            exit(1);
-        }
-
-        break;
-    case 'mail_user_get':
-        // Populate variable $session_id, $domain.
-        $arguments = unserialize($_SERVER['argv'][4]);
-        extract($arguments);
-        $client = new SoapClient(null, $options);
-        try {
-            if($session_id = $client->login($username, $password)) {
-            }
-            //* Set the function parameters.
-            $record_record = $client->mail_user_get($session_id, $params);
-            echo serialize($record_record);
-            if($client->logout($session_id)) {
-            }
-        } catch (SoapFault $e) {
-            fwrite(STDERR, 'SOAP Error: '.$e->getMessage().PHP_EOL);
-            exit(1);
-        }
-        break;
-    default:
-        fwrite(STDERR, 'Unknown mode.'.PHP_EOL);
-        exit(1);
-        break;
-}
-EOF
-)
-
-chapter Populate variable.
-remoteUserCredentialIspconfig $ISPCONFIG_REMOTE_USER_ROOT
-if [[ -z "$ispconfig_remote_user_name" || -z "$ispconfig_remote_user_password" ]];then
-    __; red Informasi credentials tidak lengkap: '`'/usr/local/share/ispconfig/credential/remote/$ISPCONFIG_REMOTE_USER_ROOT'`'.; x
-else
-    code ispconfig_remote_user_name="$ispconfig_remote_user_name"
-    code ispconfig_remote_user_password="$ispconfig_remote_user_password"
-fi
-options="$(php -r "echo serialize([
-    'location' => '"http://${ISPCONFIG_FQDN_LOCALHOST}/remote/index.php"',
-    'uri' => '"http://${ISPCONFIG_FQDN_LOCALHOST}/remote/"',
-    'trace' => 1,
-    'exceptions' => 1,
-]);")"
-credentials="$(php -r "echo serialize([
-    'username' => '"$ispconfig_remote_user_name"',
-    'password' => '"$ispconfig_remote_user_password"',
-]);")"
-____
 
 if [ -z "$ispconfig_soap_exists_sure" ];then
     chapter Test koneksi SOAP.
-    if php -r "$php" login "$options" "$credentials";then
+    code rcm-php-ispconfig soap login
+    if [ -z "$tempfile" ];then
+        tempfile=$(mktemp -p /dev/shm -t rcm-ispconfig-control-manage-email-mailbox.XXXXXX)
+    fi
+    if rcm-php-ispconfig soap login 2> "$tempfile";then
+        while IFS= read line; do e "$line"; _.; done < "$tempfile"
         __ Login berhasil.
     else
+        rm "$tempfile"
         error Login gagal; x
     fi
     ____
@@ -407,22 +235,23 @@ fi
 
 user="$name"
 host="$domain"
-chapter Mengecek mailbox "$user"@"$host"
-if isEmailIspconfigExist "$user" "$host";then
-    __ Email "$user"@"$host" already exists.
-    __; magenta mailuser_id=$mailuser_id; _.
-elif insertEmailIspconfig "$user" "$host";then
-    __; green "$user"@"$host" created.; _.
-    __; magenta mailuser_id=$mailuser_id; _.
+email="${user}@${host}"
+chapter Autocreate mailbox '`'$email'`' di Module Mail ISPConfig.
+if [ -z "$tempfile" ];then
+    tempfile=$(mktemp -p /dev/shm -t rcm-ispconfig-control-manage-email-mailbox.XXXXXX)
+fi
+if isExists;then
+    __ Mailbox '`'$email'`' telah terdaftar di ISPConfig.
+elif create "$@";then
+    success Mailbox '`'$email'`' berhasil terdaftar di ISPConfig.
 else
-    __; red Email "$user"@"$host" failed to create.; x
+    error Mailbox '`'$email'`' gagal terdaftar di ISPConfig.; x
 fi
 ____
 
 exit 0
 
 # parse-options.sh \
-# --without-end-options-double-dash \
 # --compact \
 # --clean \
 # --no-hash-bang \
