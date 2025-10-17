7a7f5f927b152b8a3dabdccd6c6e45e29ae66de0
ijortengab <m_roji28@yahoo.com>
2025-10-17 09:07:01 +0700

    [rcm-ispconfig-setup-smtpd-certificate.sh] Bugfix. Value original akan diganti menjadi prefer value yakni sesuai dengan behaviour bawaan ISPConfig. Misalnya pada path /etc/postfix/smtpd.cert.

--- rcm/ispconfig/rcm-ispconfig-setup-smtpd-certificate.sh
+++ rcm/ispconfig/rcm-ispconfig-setup-smtpd-certificate.sh
@@ -233,12 +233,14 @@ verifyKey() {
     local output=$2
     case "$key" in
         smtpd_tls_cert_file)
-            if [[ "$output" == "smtpd_tls_cert_file = ${smtpd_tls_cert_file}" ]];then
+            prefer_parameter="prefer_${key}"
+            if [[ "$output" == "${key} = ${!prefer_parameter}" ]];then
                 return 0
             fi
             ;;
         smtpd_tls_key_file)
-            if [[ "$output" == "smtpd_tls_key_file = ${smtpd_tls_key_file}" ]];then
+            prefer_parameter="prefer_${key}"
+            if [[ "$output" == "${key} = ${!prefer_parameter}" ]];then
                 return 0
             fi
             ;;
@@ -266,23 +268,18 @@ smtpd_tls_cert_file=$(postconf -n smtpd_tls_cert_file | grep -o -P '^smtpd_tls_c
 smtpd_tls_key_file=$(postconf -n smtpd_tls_key_file | grep -o -P '^smtpd_tls_key_file\s+=\s+\K([^;]+)')
 code smtpd_tls_cert_file="$smtpd_tls_cert_file"
 code smtpd_tls_key_file="$smtpd_tls_key_file"
-[ -z "$smtpd_tls_cert_file" ] && smtpd_tls_cert_file="${POSTFIX_CONFIG_DIR}/smtpd.cert"
-[ -z "$smtpd_tls_key_file" ] && smtpd_tls_key_file="${POSTFIX_CONFIG_DIR}/smtpd.key"
-code smtpd_tls_cert_file="$smtpd_tls_cert_file"
-code smtpd_tls_key_file="$smtpd_tls_key_file"
+prefer_smtpd_tls_cert_file="${POSTFIX_CONFIG_DIR}/smtpd.cert"
+prefer_smtpd_tls_key_file="${POSTFIX_CONFIG_DIR}/smtpd.key"
+code prefer_smtpd_tls_cert_file="$prefer_smtpd_tls_cert_file"
+code prefer_smtpd_tls_key_file="$prefer_smtpd_tls_key_file"
 ____
 
-link_symbolic "$tls_certificate" "$smtpd_tls_cert_file" - absolute
-link_symbolic "$tls_certificate_key" "$smtpd_tls_key_file" - absolute
+link_symbolic "$tls_certificate" "$prefer_smtpd_tls_cert_file" - absolute
+link_symbolic "$tls_certificate_key" "$prefer_smtpd_tls_key_file" - absolute
 
-path="${POSTFIX_CONFIG_DIR}/smtpd.key"
-[ -f $path ] || fileMustExists $path
-path="${POSTFIX_CONFIG_DIR}/smtpd.cert"
-[ -f $path ] || fileMustExists $path
-tempfile_error=$(mktemp -p /dev/shm -t rcm-postfix-multiple-certificate.XXXXXX)
-tempfile_output=$(mktemp -p /dev/shm -t rcm-postfix-multiple-certificate.XXXXXX)
+tempfile_error=$(mktemp -p /dev/shm -t rcm-ispconfig-setup-smtpd-certificate.XXXXXX)
+tempfile_output=$(mktemp -p /dev/shm -t rcm-ispconfig-setup-smtpd-certificate.XXXXXX)
 
-restart=
 for key in smtpd_tls_cert_file smtpd_tls_key_file; do
     chapter Memastikan key '`'$key'`' enabled.
     postconf -n $key 2> $tempfile_error > $tempfile_output
@@ -292,7 +289,6 @@ for key in smtpd_tls_cert_file smtpd_tls_key_file; do
     fi
     output="$(<"$tempfile_output")"
     found=
-
     if [ -n "$output" ];then
         __ Key ditemukan.
         e "$output"; _.
@@ -307,11 +303,10 @@ for key in smtpd_tls_cert_file smtpd_tls_key_file; do
             postconf -# $key
         fi
     fi
-    ____
-
     if [ -z "$found" ];then
         __ Set value of key '`'$key'`'
-        postconf "${key}=${!key}"
+        prefer_parameter="prefer_${key}"
+        postconf "${key}=${!prefer_parameter}"
         __ Verifikasi.
         if verifyKey "$key" "$(postconf -n $key)";then
             __ Verifikasi berhasil.
@@ -319,16 +314,19 @@ for key in smtpd_tls_cert_file smtpd_tls_key_file; do
         else
             error Verifikasi gagal.; rm $tempfile_error; rm $tempfile_output; x
         fi
-
     fi
+    ____
 done
 
 if [ -n "$restart" ];then
     chapter Restart Postfix
     code systemctl restart postfix
     systemctl restart postfix
+    ____
 fi
-____
+
+rm $tempfile_error
+rm $tempfile_output
 
 exit 0
 
