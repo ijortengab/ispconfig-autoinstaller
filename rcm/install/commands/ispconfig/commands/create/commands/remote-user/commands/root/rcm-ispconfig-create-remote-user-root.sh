24cd0a0a69ae2783aa38e66e1bbeec6ae732a927
ijortengab <m_roji28@yahoo.com>
2026-06-17 10:06:11 +0700

    Set version 0.11.0-alpha.4.

--- rcm/install/commands/ispconfig/commands/create/commands/remote-user/commands/root/rcm-ispconfig-create-remote-user-root.sh
+++ rcm/install/commands/ispconfig/commands/create/commands/remote-user/commands/root/rcm-ispconfig-create-remote-user-root.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RCM_EXTENSION_VERSION=0.11.0-alpha.3
+RCM_EXTENSION_VERSION=0.11.0-alpha.4
 
 # Usage Functions.
 usage() {
--- rcm/php/rcm-php-ispconfig.php
+++ rcm/php/rcm-php-ispconfig.php
@@ -13,7 +13,7 @@ function _die($string='', $code = 1) {
     exit($code);
 }
 function printVersion() {
-    echo '0.11.0-alpha.3';
+    echo '0.11.0-alpha.4';
 }
 // Clone value.
 $arguments_count = $argc;
