b66f71c55f118f30aee4f4afe897820e68a1913c
ijortengab <m_roji28@yahoo.com>
2026-06-18 11:02:11 +0700

    [rcm-ispconfig-autoinstaller-nginx.sh] Pindah sebagian code terkait modifikasi setup debian11. Add class debian11 implements ispconfig/os-setup interface with method setup-trait-modify().

---
+++ rcm/install/interfaces/ispconfig/os-setup/implements/debian11/methods/setup-trait-modify/socket.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+modifyFileDebian11() {
+    local file=/tmp/ispconfig3_install/install/dist/conf/debian110.conf.php
+    isFileExists "$file"
+    [ -n "$notfound" ] && fileMustExists "$file"
+    if [[ ! "$php_version" == 7.4 ]];then
+        sed -i \
+            -e 's,"7\.4","'$php_version'",g' \
+            -e 's,/7\.4/,/'$php_version'/,g' \
+            -e 's,php7\.4,php'$php_version',g' \
+            "$file"
+    fi
+    # Edit informasi cron dan ufw yang terlewat.
+    string="//* cron"
+    number_1=$(grep -n -F "$string" "$file" | head -1 | cut -d: -f1)
+    number_1plus=$((number_1 - 1))
+    number_1plus2=$((number_1 + 1))
+    part1=$(sed -n '1,'$number_1plus'p' "$file")
+    part2=$(sed -n $number_1plus2',$p' "$file")
+    additional=$(cat << 'EOF'
+
+//* ufw
+$conf['ufw']['installed'] = false;
+
+//* cron
+$conf['cron']['installed'] = false;
+EOF
+        )
+    echo "$part1"$'\n'"$additional"$'\n'"$part2" > "$file"
+}
