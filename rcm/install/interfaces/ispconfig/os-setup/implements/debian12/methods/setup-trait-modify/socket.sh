d5b8ac1f64f845241fb9a15ce0951884147b648e
ijortengab <m_roji28@yahoo.com>
2026-06-18 11:05:56 +0700

    [rcm-ispconfig-autoinstaller-nginx.sh] Pindah sebagian code terkait modifikasi setup debian12. Add class debian12 implements ispconfig/os-setup interface with method setup-trait-modify().

---
+++ rcm/install/interfaces/ispconfig/os-setup/implements/debian12/methods/setup-trait-modify/socket.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+modifyFileDebian12() {
+    local file=/tmp/ispconfig3_install/install/dist/conf/debian120.conf.php
+    isFileExists "$file"
+    [ -n "$notfound" ] && fileMustExists "$file"
+    if [[ ! "$php_version" == 8.2 ]];then
+        sed -i \
+            -e 's,"8\.2","'$php_version'",g' \
+            -e 's,/8\.2/,/'$php_version'/,g' \
+            -e 's,php8\.2,php'$php_version',g' \
+            "$file"
+    fi
+    # Edit informasi cron dan ufw yang terlewat.
+    string="//* cron"
+    number_1=$(grep -n -F "$string" "$file" | head -1 | cut -d: -f1)
+    number_1plus=$((number_1 - 1))
+    number_1plus2=$((number_1 + 1))
+    part1=$(sed -n '1,'$number_1plus'p' "$file")
+    part2=$(sed -n $number_1plus2',$p' "$file")
+    additional=$(cat << 'EOF'
+
+//* ufw
+$conf['ufw']['installed'] = false;
+
+//* cron
+$conf['cron']['installed'] = false;
+EOF
+        )
+    echo "$part1"$'\n'"$additional"$'\n'"$part2" > "$file"
+}
+createFileDebian12() {
+    local source=/tmp/ispconfig3_install/install/dist/conf/debian110.conf.php
+    local file=/tmp/ispconfig3_install/install/dist/conf/debian120.conf.php
+    if [ ! -f "$file" ];then
+        fileMustExists "$source"
+        __ Membuat file '`'debian120.conf.php'`'.
+        cp "$source" "$file"
+        fileMustExists "$file"
+        sed -i \
+            -e 's,Debian 11,Debian 12,g' \
+            -e 's,debian110,debian120,g' \
+            -e 's,"7\.4","'$php_version'",g' \
+            -e 's,/7\.4/,/'$php_version'/,g' \
+            -e 's,php7\.4,php'$php_version',g' \
+            "$file"
+    fi
+    # Edit informasi cron dan ufw yang terlewat.
+    string="//* cron"
+    number_1=$(grep -n -F "$string" "$file" | head -1 | cut -d: -f1)
+    number_1plus=$((number_1 - 1))
+    number_1plus2=$((number_1 + 1))
+    part1=$(sed -n '1,'$number_1plus'p' "$file")
+    part2=$(sed -n $number_1plus2',$p' "$file")
+    additional=$(cat << 'EOF'
+
+//* ufw
+$conf['ufw']['installed'] = false;
+
+//* cron
+$conf['cron']['installed'] = false;
+EOF
+        )
+    echo "$part1"$'\n'"$additional"$'\n'"$part2" > "$file"
+}
+editInstallLibDebian12() {
+    file=/tmp/ispconfig3_install/install/lib/install.lib.php
+    string="elseif(substr(trim(file_get_contents('/etc/debian_version')),0,2) == '12')"
+    edit=1
+    if grep -q -F "$string" "$file";then
+        __ File sudah diedit agar terdapat informasi Debian 12: $(basename "$file").
+        edit=
+    fi
+    if [ -n "$edit" ];then
+        __ Mengedit file: $(basename "$file").
+        string="elseif(substr(trim(file_get_contents('/etc/debian_version')),0,2) == '11')"
+        number_1=$(grep -n -F "$string" "$file" | head -1 | cut -d: -f1)
+        number_1plus=$((number_1 + 6))
+        number_1plus2=$((number_1 + 7))
+        part1=$(sed -n '1,'$number_1plus'p' "$file")
+        part2=$(sed -n $number_1plus2',$p' "$file")
+        additional=$(cat << 'EOF'
+            } elseif(substr(trim(file_get_contents('/etc/debian_version')),0,2) == '12') {
+                $distname = 'Debian';
+                $distver = 'Bookworm';
+                $distconfid = 'debian120';
+                $distid = 'debian60';
+                $distbaseid = 'debian';
+                swriteln("Operating System: Debian 12.0 (Bookworm) or compatible\n");
+EOF
+        )
+        echo "$part1"$'\n'"$additional"$'\n'"$part2" > "$file"
+        __ Verifikasi.
+        if grep -q -F "$string" "$file";then
+            __; green File berhasil diedit agar terdapat informasi Debian 12: $(basename "$file").; _.
+        else
+            __; red File gagal diedit: $(basename "$file"); _.
+        fi
+    fi
+}
